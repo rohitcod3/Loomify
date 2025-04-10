7710709ce0191104f7a58351bf48a68ee9e0d205
rohitcod3 <rohit.bangar9@gmail.com>
2025-04-09 23:58:42 -0400

    add Zod validation schema for moving videos

--- src/hooks/useFolders.ts
+++ src/hooks/useFolders.ts
@@ -1,8 +1,9 @@
 import { useAppSelector } from "@/redux/store"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { useMutationData } from "./useMutationData"
-import { moveVideoLocation } from "@/actions/workspace"
+import { getWorkspaceFolders, moveVideoLocation } from "@/actions/workspace"
 import useZodForm from "./useZodForm"
+import { moveVideoSchema } from "@/components/forms/change-video-location/schema"
 
 export const  useMoveVideos = (videoId: string, currentWorkspace: string)=>{
     const {folders} = useAppSelector((state) => state.FolderReducer)
@@ -21,9 +22,37 @@ export const  useMoveVideos = (videoId: string, currentWorkspace: string)=>{
 undefined>(undefined)
 
 const {mutate,isPending} = useMutationData(['change-video-location'],
-    (data:{folder_id:string, workspace_id:string})=>{
+    (data:{folder_id:string, workspace_id:string})=>
         moveVideoLocation(videoId,data.workspace_id, data.folder_id )
-    }
+    
 )
-const {} = useZodForm()
+const {errors, onFormSubmit, watch, register} = useZodForm(
+moveVideoSchema,
+mutate,
+{folder_id: null, workspace_id: currentWorkspace}
+)
+
+const fetchFolders = async(workspace:string) => {
+    setIsFetching(true)
+    const folders  = await getWorkspaceFolders(workspace)
+    setIsFetching (false)
+    setIsFolders(folders.data)
+}
+useEffect(() => {
+    fetchFolders(currentWorkspace)
+},[])
+
+
+useEffect(()=> {
+    const workspace = watch(async(value => {
+        if(value.workspace._id) fetchFolders(value.workspace_id)
+    })
+
+return () => workspace.uusubscribe();
+)
+}, [watch])
+
+
+
+
 }
