913602f9df74e8cda0df20958893427a8e59ecc7
rohitcod3 <rohit.bangar9@gmail.com>
2025-04-20 16:46:43 -0400

    fixed ts function export error

--- src/app/dashboard/[workspaceId]/folder/[folderId]/page.tsx
+++ src/app/dashboard/[workspaceId]/folder/[folderId]/page.tsx
@@ -1,13 +1,17 @@
-
 import { getAllUserVideos, getFolderInfo } from '@/actions/workspace'
 import FolderInfo from '@/components/global/folders/folder-info'
 import Videos from '@/components/global/videos'
 import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query'
 import React from 'react'
 
-type Props = {params: {folderId:string, workspaceId:string}}
+type Props = {
+  params: {
+    folderId: string;
+    workspaceId: string;
+  };
+};
 
-const page = async ({ params }: { params: { folderId: string; workspaceId: string } }) => {
+export default async function Page({ params }: Props) {
   const { folderId, workspaceId } = params;
 
   const query = new QueryClient();
@@ -25,7 +29,7 @@ const page = async ({ params }: { params: { folderId: string; workspaceId: strin
   await query.prefetchQuery({
     queryKey: ['workspace-videos'],
     queryFn: () => getAllUserVideos(workspaceId)
-  })
+  });
 
   return (
     <HydrationBoundary state={dehydrate(query)}>
@@ -37,6 +41,4 @@ const page = async ({ params }: { params: { folderId: string; workspaceId: strin
       />
     </HydrationBoundary>
   );
-};
-
-export default page;
+}
