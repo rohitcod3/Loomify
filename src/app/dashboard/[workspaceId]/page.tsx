de09c02222d8f5e56d233dd2dfdf6db4901401e1
rohitcod3 <rohit.bangar9@gmail.com>
2025-04-20 19:46:55 -0400

    update [workspaceId] route to await async params as required in Next.js 15

--- src/app/dashboard/[workspaceId]/page.tsx
+++ src/app/dashboard/[workspaceId]/page.tsx
@@ -1,16 +1,19 @@
-import { getAllUserVideos } from '@/actions/workspace'
-import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query'
 import CreateFolders from '@/components/global/create-folders'
 import CreateWorkspace from '@/components/global/create-workspace'
 import Folders from '@/components/global/folders'
 import Videos from '@/components/global/videos'
 import { Tabs, TabsContent, TabsList } from '@/components/ui/tabs'
 import { TabsTrigger } from '@radix-ui/react-tabs'
+import { Video } from 'lucide-react'
 import React from 'react'
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query'
+import { getAllUserVideos } from '@/actions/workspace'
 import Link from 'next/link'
 
-export default async function Page({ params }: { params: any }) {
-  const { workspaceId } = await params as { workspaceId: string }
+export default async function Page(props: {
+  params: Promise<{ workspaceId: string }>
+}) {
+  const { workspaceId } = await props.params
 
   const queryClient = new QueryClient()
 
@@ -25,10 +28,16 @@ export default async function Page({ params }: { params: any }) {
         <Tabs defaultValue="videos" className="mt-6">
           <div className="flex w-full justify-between items-center">
             <TabsList className="bg-transparent gap-2 pl-0">
-              <TabsTrigger className="p-[13px] px-6 rounded-full data-[state=active]:bg-[#252525]" value="videos">
+              <TabsTrigger
+                className="p-[13px] px-6 rounded-full data-[state=active]:bg-[#252525]"
+                value="videos"
+              >
                 Videos
               </TabsTrigger>
-              <TabsTrigger className="p-[13px] px-6 rounded-full data-[state=active]:bg-[#252525]" value="archive">
+              <TabsTrigger
+                className="p-[13px] px-6 rounded-full data-[state=active]:bg-[#252525]"
+                value="archive"
+              >
                 Archive
               </TabsTrigger>
             </TabsList>
