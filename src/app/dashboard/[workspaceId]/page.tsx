a71b3980b55d91390eaf24e9d7ac61048f836e70
rohitcod3 <rohit.bangar9@gmail.com>
2025-04-20 18:05:43 -0400

    patch type error in [workspaceId] page route

--- src/app/dashboard/[workspaceId]/page.tsx
+++ src/app/dashboard/[workspaceId]/page.tsx
@@ -1,27 +1,23 @@
+import { getAllUserVideos } from '@/actions/workspace'
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query'
 import CreateFolders from '@/components/global/create-folders'
 import CreateWorkspace from '@/components/global/create-workspace'
 import Folders from '@/components/global/folders'
 import Videos from '@/components/global/videos'
 import { Tabs, TabsContent, TabsList } from '@/components/ui/tabs'
 import { TabsTrigger } from '@radix-ui/react-tabs'
-import { Video } from 'lucide-react'
 import React from 'react'
-import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query'
-import { getAllUserVideos } from '@/actions/workspace'
 import Link from 'next/link'
 
-type Props = {
-  params: { workspaceId: string }
-}
+export default async function Page({ params }: { params: any }) {
+  const { workspaceId } = await params as { workspaceId: string }
 
-export default async function Page({ params }: Props) {
-  const { workspaceId } = params;
+  const queryClient = new QueryClient()
 
-  const queryClient = new QueryClient();
   await queryClient.prefetchQuery({
     queryKey: ['workspace-videos'],
     queryFn: () => getAllUserVideos(workspaceId),
-  });
+  })
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
@@ -29,16 +25,10 @@ export default async function Page({ params }: Props) {
         <Tabs defaultValue="videos" className="mt-6">
           <div className="flex w-full justify-between items-center">
             <TabsList className="bg-transparent gap-2 pl-0">
-              <TabsTrigger
-                className="p-[13px] px-6 rounded-full data-[state=active]:bg-[#252525]"
-                value="videos"
-              >
+              <TabsTrigger className="p-[13px] px-6 rounded-full data-[state=active]:bg-[#252525]" value="videos">
                 Videos
               </TabsTrigger>
-              <TabsTrigger
-                className="p-[13px] px-6 rounded-full data-[state=active]:bg-[#252525]"
-                value="archive"
-              >
+              <TabsTrigger className="p-[13px] px-6 rounded-full data-[state=active]:bg-[#252525]" value="archive">
                 Archive
               </TabsTrigger>
             </TabsList>
