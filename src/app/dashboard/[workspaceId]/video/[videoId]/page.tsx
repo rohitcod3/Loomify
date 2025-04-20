921d63a43c80499bc4fe76ab4fbcc7559df2041f
rohitcod3 <rohit.bangar9@gmail.com>
2025-04-20 19:30:07 -0400

    patch dynamic route build error in preview/[videoId] page

--- src/app/dashboard/[workspaceId]/video/[videoId]/page.tsx
+++ src/app/dashboard/[workspaceId]/video/[videoId]/page.tsx
@@ -3,19 +3,17 @@ import VideoPreview from '@/components/global/videos/preview'
 import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query'
 import React from 'react'
 
-export default async function Page({ params }: { params: any }) {
-  const { videoId } = await params as { videoId: string }
-
+export default async function Page(props: any) {
   const queryClient = new QueryClient()
 
   await queryClient.prefetchQuery({
     queryKey: ['preview-video'],
-    queryFn: () => getPreviewVideo(videoId),
+    queryFn: () => getPreviewVideo(props.params.videoId),
   })
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <VideoPreview videoId={videoId} />
+      <VideoPreview videoId={props.params.videoId} />
     </HydrationBoundary>
   )
 }
