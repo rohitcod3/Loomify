8b8d0d24a05d643653a7568ffd2c9abff44b5bc2
rohitcod3 <rohit.bangar9@gmail.com>
2025-04-20 17:44:58 -0400

    fix ts error 4

--- src/app/dashboard/[workspaceId]/folder/[folderId]/page.tsx
+++ src/app/dashboard/[workspaceId]/folder/[folderId]/page.tsx
@@ -1,42 +1,44 @@
-
 import { getAllUserVideos, getFolderInfo } from '@/actions/workspace'
 import FolderInfo from '@/components/global/folders/folder-info'
 import Videos from '@/components/global/videos'
 import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query'
 import React from 'react'
 
-type Props = {params: {folderId:string, workspaceId:string}}
+type Props = {
+  params: {
+    folderId: string
+    workspaceId: string
+  }
+}
 
-export default async function Page(props : Props)  {
-  const { folderId, workspaceId } = await props.params;
+export default async function Page({ params }: Props) {
+  const { folderId, workspaceId } = await params
 
-  const query = new QueryClient();
+  const queryClient = new QueryClient()
 
-  await query.prefetchQuery({
+  await queryClient.prefetchQuery({
     queryKey: ['folder-videos'],
     queryFn: () => getAllUserVideos(folderId),
-  });
+  })
 
-  await query.prefetchQuery({
+  await queryClient.prefetchQuery({
     queryKey: ['folder-info'],
     queryFn: () => getFolderInfo(folderId),
-  });
+  })
 
-  await query.prefetchQuery({
+  await queryClient.prefetchQuery({
     queryKey: ['workspace-videos'],
-    queryFn: () => getAllUserVideos(workspaceId)
+    queryFn: () => getAllUserVideos(workspaceId),
   })
 
   return (
-    <HydrationBoundary state={dehydrate(query)}>
+    <HydrationBoundary state={dehydrate(queryClient)}>
       <FolderInfo folderId={folderId} />
       <Videos 
-        workspaceId={workspaceId} 
-        folderId={folderId} 
-        videosKey='folder-videos' 
+        workspaceId={workspaceId}
+        folderId={folderId}
+        videosKey="folder-videos"
       />
     </HydrationBoundary>
-  );
-};
-
-
+  )
+}
